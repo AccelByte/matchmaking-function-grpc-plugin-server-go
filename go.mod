f1b0ec8c5ea8e6d5f7fd0b868841e9a299f5086e
ruli <ruli@accelbyte.net>
2022-11-04 13:31:07 +0700

    feat: add token validator

--- go.mod
+++ go.mod
@@ -2,10 +2,15 @@ module plugin-arch-grpc-server-go
 
 go 1.18
 
+replace github.com/AccelByte/accelbyte-go-sdk => /home/ruli@accelbyte.net/Workspace/Bitbucket/justice-codegen-go-sdk
+
 require (
+	github.com/AccelByte/accelbyte-go-sdk v0.30.0
+	github.com/AccelByte/bloom v0.0.0-20180915202807-98c052463922
+	github.com/AccelByte/go-jose v2.1.4+incompatible
 	github.com/emicklei/go-restful v2.16.0+incompatible
 	github.com/golang/protobuf v1.5.2
-	github.com/google/uuid v1.1.2
+	github.com/google/uuid v1.3.0
 	github.com/parnurzeal/gorequest v0.2.16
 	github.com/sirupsen/logrus v1.9.0
 	github.com/smartystreets/goconvey v1.7.2 // indirect
@@ -18,23 +23,50 @@ require (
 	google.golang.org/protobuf v1.28.1
 )
 
-require google.golang.org/grpc/examples v0.0.0-20221102000843-040b795b51ea
+require (
+	github.com/pkg/errors v0.9.1
+	google.golang.org/grpc/examples v0.0.0-20221102000843-040b795b51ea
+)
 
 require (
+	github.com/PuerkitoBio/purell v1.1.1 // indirect
+	github.com/PuerkitoBio/urlesc v0.0.0-20170810143723-de5bf2ad4578 // indirect
+	github.com/asaskevich/govalidator v0.0.0-20200907205600-7a23bdc65eef // indirect
 	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/elazarl/goproxy v0.0.0-20220901064549-fbd10ff4f5a1 // indirect
 	github.com/go-logr/logr v1.2.3 // indirect
 	github.com/go-logr/stdr v1.2.2 // indirect
+	github.com/go-openapi/analysis v0.20.0 // indirect
+	github.com/go-openapi/errors v0.20.0 // indirect
+	github.com/go-openapi/jsonpointer v0.19.5 // indirect
+	github.com/go-openapi/jsonreference v0.19.5 // indirect
+	github.com/go-openapi/loads v0.20.2 // indirect
+	github.com/go-openapi/runtime v0.19.29 // indirect
+	github.com/go-openapi/spec v0.20.3 // indirect
+	github.com/go-openapi/strfmt v0.20.1 // indirect
+	github.com/go-openapi/swag v0.19.15 // indirect
+	github.com/go-openapi/validate v0.20.2 // indirect
+	github.com/go-stack/stack v1.8.0 // indirect
+	github.com/josharian/intern v1.0.0 // indirect
 	github.com/json-iterator/go v1.1.12 // indirect
+	github.com/mailru/easyjson v0.7.6 // indirect
+	github.com/mitchellh/mapstructure v1.4.1 // indirect
 	github.com/modern-go/concurrent v0.0.0-20180228061459-e0a39a4cb421 // indirect
 	github.com/modern-go/reflect2 v1.0.2 // indirect
-	github.com/pkg/errors v0.9.1 // indirect
+	github.com/oklog/ulid v1.3.1 // indirect
+	github.com/opentracing/opentracing-go v1.2.0 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
+	github.com/spaolacci/murmur3 v1.1.0 // indirect
+	github.com/willf/bitset v1.1.11 // indirect
+	go.mongodb.org/mongo-driver v1.5.1 // indirect
+	go.opentelemetry.io/contrib/propagators/aws v1.4.0 // indirect
 	go.opentelemetry.io/otel/trace v1.10.0 // indirect
-	golang.org/x/net v0.0.0-20201021035429-f5854403a974 // indirect
+	golang.org/x/crypto v0.0.0-20200622213623-75b288015ac9 // indirect
+	golang.org/x/net v0.0.0-20210119194325-5f4716e94777 // indirect
 	golang.org/x/sys v0.0.0-20220715151400-c0bba94af5f8 // indirect
-	golang.org/x/text v0.3.3 // indirect
+	golang.org/x/text v0.3.5 // indirect
 	google.golang.org/genproto v0.0.0-20200806141610-86f49bd18e98 // indirect
+	gopkg.in/yaml.v2 v2.4.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 	moul.io/http2curl v1.0.0 // indirect
 )
--- pkg/server/authorizationInterceptors.go
+++ pkg/server/authorizationInterceptors.go
@@ -5,15 +5,38 @@
 package server
 
 import (
-	"fmt"
-	"net/http"
-	"os"
+	"bytes"
+	"crypto/rsa"
+	"encoding/base64"
+	"encoding/binary"
+	"math/big"
 	"strings"
+	"time"
 
-	"github.com/emicklei/go-restful"
+	"github.com/AccelByte/accelbyte-go-sdk/iam-sdk/pkg/iamclient/o_auth2_0"
+	"github.com/AccelByte/accelbyte-go-sdk/iam-sdk/pkg/iamclientmodels"
+	"github.com/AccelByte/accelbyte-go-sdk/services-api/pkg/factory"
+	"github.com/AccelByte/accelbyte-go-sdk/services-api/pkg/service/iam"
+	"github.com/AccelByte/accelbyte-go-sdk/services-api/pkg/utils/auth"
+	"github.com/AccelByte/bloom"
+	"github.com/AccelByte/go-jose/jwt"
 	"github.com/google/uuid"
-	"github.com/parnurzeal/gorequest"
+	"github.com/pkg/errors"
 	"github.com/sirupsen/logrus"
+)
+
+var (
+	jwtEncoding  = base64.URLEncoding.WithPadding(base64.NoPadding)
+	keys         = make(map[string]*rsa.PublicKey)
+	revokedUsers = make(map[string]time.Time)
+	jwtClaims    = JWTClaims{}
+	configRepo   = *auth.DefaultConfigRepositoryImpl()
+	tokenRepo    = *auth.DefaultTokenRepositoryImpl()
+	oauthService = &iam.OAuth20Service{
+		Client:           factory.NewIamClient(&configRepo),
+		ConfigRepository: &configRepo,
+		TokenRepository:  &tokenRepo,
+	}
 )
 
 func ValidateAuth(authorization []string) bool {
@@ -22,7 +45,186 @@ func ValidateAuth(authorization []string) bool {
 	}
 	token := strings.TrimPrefix(authorization[0], "Bearer ")
 
-	return token == "some-secret-token"
+	return TokenValidator(token)
+}
+
+func TokenValidator(accessToken string) bool {
+	// store the accessToken
+	token := &iamclientmodels.OauthmodelTokenResponseV3{
+		AccessToken: &accessToken,
+	}
+	errStoreToken := oauthService.TokenRepository.Store(token)
+	if errStoreToken != nil {
+		logrus.Error(errStoreToken)
+
+		return false
+	}
+
+	// parse signed
+	parsedSignedToken, err := jwt.ParseSigned(accessToken)
+	if err != nil {
+		logrus.Error("validateJWT: unable to parse JWT. ", err)
+
+		return false
+	}
+	if parsedSignedToken.Headers[0].KeyID == "" {
+		logrus.Error("invalid header when parsed signed.")
+
+		return false
+	}
+
+	// fetch jwks key using public key
+	errFetchJWKS := fetchJWKS()
+	if errFetchJWKS != nil {
+		logrus.Error(errFetchJWKS)
+
+		return false
+	}
+	if keys == nil {
+		logrus.Error("public key to validate is nil")
+
+		return false
+	}
+
+	// claims
+	errClaims := parsedSignedToken.Claims(keys[parsedSignedToken.Headers[0].KeyID], &jwtClaims)
+	if errClaims != nil {
+		logrus.Error(errClaims)
+
+		return false
+	}
+
+	// checks expiration time
+	err = jwtClaims.Validate()
+	if err != nil {
+		if err == jwt.ErrExpired {
+			logrus.Error("token expired. ", errClaims)
+
+			return false
+		}
+		logrus.Error("unable to validate JWT. ", err)
+
+		return false
+	}
+
+	// get revocation list
+	errRevocationList := getRevocationList(accessToken)
+	if errRevocationList != nil {
+		logrus.Error(errRevocationList)
+
+		return false
+	}
+
+	// check if user is in revocation list
+	for userID, revokedAt := range revokedUsers {
+		if userID == jwtClaims.Subject {
+			logrus.Error("user is found but token is revoked.")
+
+			return false
+		}
+		if revokedAt.Unix() >= int64(jwtClaims.IssuedAt) {
+			logrus.Error("token is revoked because of issued time")
+
+			return false
+		}
+	}
+
+	logrus.Info("JWT validated.")
+
+	return true
+}
+
+func fetchJWKS() error {
+	input := &o_auth2_0.GetJWKSV3Params{}
+	getJWKSV3, err := oauthService.GetJWKSV3Short(input)
+	if err != nil {
+		logrus.Error(err)
+
+		return nil
+	}
+
+	for _, key := range getJWKSV3.Keys {
+		publicKey, errGenerate := generatePublicKey(key)
+		if errGenerate != nil {
+			logrus.Error(errGenerate)
+
+			return errGenerate
+		}
+		keys[key.Kid] = publicKey
+	}
+
+	return nil
+}
+
+func generatePublicKey(jwk *iamclientmodels.OauthcommonJWKKey) (*rsa.PublicKey, error) {
+	n, err := getModulus(jwk.N)
+	if err != nil {
+		return nil, err
+	}
+
+	e, err := getPublicExponent(jwk.E)
+	if err != nil {
+		return nil, err
+	}
+
+	return &rsa.PublicKey{N: n, E: e}, nil
+}
+
+func getModulus(jwkN string) (*big.Int, error) {
+	decodedN, err := jwtEncoding.DecodeString(jwkN)
+	if err != nil {
+		return nil, errors.Wrap(err, "getModulus: unable to decode JWK modulus string")
+	}
+
+	n := big.NewInt(0)
+	n.SetBytes(decodedN)
+
+	return n, nil
+}
+
+func getPublicExponent(jwkE string) (int, error) {
+	decodedE, err := jwtEncoding.DecodeString(jwkE)
+	if err != nil {
+		return 0, errors.Wrap(err, "getPublicExponent: unable to decode JWK exponent string")
+	}
+
+	var eBytes []byte
+	if len(eBytes) < 8 {
+		eBytes = make([]byte, 8-len(decodedE), 8)
+		eBytes = append(eBytes, decodedE...)
+	} else {
+		eBytes = decodedE
+	}
+
+	eReader := bytes.NewReader(eBytes)
+
+	var e uint64
+
+	err = binary.Read(eReader, binary.BigEndian, &e)
+	if err != nil {
+		return 0, errors.Wrap(err, "getPublicExponent: unable to read JWK exponent bytes")
+	}
+
+	return int(e), nil
+}
+
+func getRevocationList(accessToken string) error {
+	input := &o_auth2_0.GetRevocationListV3Params{}
+	revocationList, err := oauthService.GetRevocationListV3Short(input)
+	if err != nil {
+		return err
+	}
+
+	// revoked token
+	filter := bloom.From(revocationList.RevokedTokens.Bits, uint(*revocationList.RevokedTokens.K))
+	filter.MightContain([]byte(accessToken))
+
+	// revoked user
+	for _, revokedUser := range revocationList.RevokedUsers {
+		revokedUsers[*revokedUser.ID] = time.Time(revokedUser.RevokedAt)
+	}
+
+	return nil
 }
 
 // GenerateUUID generates uuid without hyphens
@@ -30,33 +232,4 @@ func GenerateUUID() string {
 	id, _ := uuid.NewRandom()
 
 	return strings.ReplaceAll(id.String(), "-", "")
-}
-
-func GetToken(username string, password string) string {
-	testNamespace := os.Getenv("AB_NAMESPACE")
-	encodedAdminClientID := os.Getenv("ENCODED_CLIENT_ID")
-	url := "https://demo.accelbyte.io/iam/v3/oauth/token"
-	data := fmt.Sprintf("grant_type=password&username=%s&password=%s&namespace=%s", username, password, testNamespace)
-
-	result := struct {
-		Token string `json:"access_token"`
-	}{}
-	authorization := fmt.Sprintf("Basic %s", encodedAdminClientID)
-	resp, _, errs := gorequest.New().
-		Post(url).
-		Set("Authorization", authorization).
-		Set("Accept", restful.MIME_JSON).
-		Type(gorequest.TypeForm).
-		Send(data).
-		EndStruct(&result)
-	if len(errs) != 0 {
-		logrus.Error(errs)
-
-		return ""
-	}
-	if resp.StatusCode != http.StatusOK {
-		return ""
-	}
-
-	return result.Token
 }
