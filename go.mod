62bd650eb44ee2993cffc58c775625bc5fc26501
ruli <ruli@accelbyte.net>
2022-11-16 13:44:53 +0700

    feat: add metrics for prometheus

--- go.mod
+++ go.mod
@@ -7,8 +7,10 @@ require (
 	github.com/AccelByte/bloom v0.0.0-20180915202807-98c052463922
 	github.com/AccelByte/go-jose v2.1.4+incompatible
 	github.com/google/uuid v1.3.0
+	github.com/grpc-ecosystem/go-grpc-prometheus v1.2.0
 	github.com/patrickmn/go-cache v2.1.0+incompatible
 	github.com/pkg/errors v0.9.1
+	github.com/prometheus/client_golang v1.14.0
 	github.com/sirupsen/logrus v1.9.0
 	github.com/stretchr/testify v1.8.0
 	go.opentelemetry.io/contrib/instrumentation/google.golang.org/grpc/otelgrpc v0.36.1
@@ -16,6 +18,7 @@ require (
 	go.opentelemetry.io/otel v1.11.1
 	go.opentelemetry.io/otel/exporters/otlp/otlptrace/otlptracegrpc v1.11.1
 	go.opentelemetry.io/otel/sdk v1.11.1
+	go.opentelemetry.io/otel/sdk/metric v0.33.0
 	google.golang.org/grpc v1.50.1
 	google.golang.org/protobuf v1.28.1
 )
@@ -24,7 +27,9 @@ require (
 	github.com/PuerkitoBio/purell v1.1.1 // indirect
 	github.com/PuerkitoBio/urlesc v0.0.0-20170810143723-de5bf2ad4578 // indirect
 	github.com/asaskevich/govalidator v0.0.0-20200907205600-7a23bdc65eef // indirect
+	github.com/beorn7/perks v1.0.1 // indirect
 	github.com/cenkalti/backoff/v4 v4.1.3 // indirect
+	github.com/cespare/xxhash/v2 v2.1.2 // indirect
 	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/go-logr/logr v1.2.3 // indirect
 	github.com/go-logr/stdr v1.2.2 // indirect
@@ -43,20 +48,25 @@ require (
 	github.com/grpc-ecosystem/grpc-gateway/v2 v2.7.0 // indirect
 	github.com/josharian/intern v1.0.0 // indirect
 	github.com/mailru/easyjson v0.7.6 // indirect
+	github.com/matttproud/golang_protobuf_extensions v1.0.1 // indirect
 	github.com/mitchellh/mapstructure v1.4.1 // indirect
 	github.com/oklog/ulid v1.3.1 // indirect
 	github.com/opentracing/opentracing-go v1.2.0 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
+	github.com/prometheus/client_model v0.3.0 // indirect
+	github.com/prometheus/common v0.37.0 // indirect
+	github.com/prometheus/procfs v0.8.0 // indirect
 	github.com/spaolacci/murmur3 v0.0.0-20180118202830-f09979ecbc72 // indirect
 	github.com/willf/bitset v1.1.11 // indirect
 	go.mongodb.org/mongo-driver v1.5.1 // indirect
 	go.opentelemetry.io/contrib/propagators/aws v1.4.0 // indirect
 	go.opentelemetry.io/otel/exporters/otlp/internal/retry v1.11.1 // indirect
 	go.opentelemetry.io/otel/exporters/otlp/otlptrace v1.11.1 // indirect
+	go.opentelemetry.io/otel/metric v0.33.0 // indirect
 	go.opentelemetry.io/otel/trace v1.11.1 // indirect
 	go.opentelemetry.io/proto/otlp v0.19.0 // indirect
 	golang.org/x/crypto v0.0.0-20200622213623-75b288015ac9 // indirect
-	golang.org/x/net v0.0.0-20210813160813-60bc85c4be6d // indirect
+	golang.org/x/net v0.0.0-20220225172249-27dd8689420f // indirect
 	golang.org/x/sys v0.0.0-20220919091848-fb04ddd9f9c8 // indirect
 	golang.org/x/text v0.3.7 // indirect
 	google.golang.org/genproto v0.0.0-20220329172620-7be39ac1afc7 // indirect
--- main.go
+++ main.go
@@ -8,46 +8,54 @@ import (
 	"context"
 	"flag"
 	"fmt"
-	"log"
 	"net"
 	"net/http"
+	"os"
+	"os/signal"
 	"time"
 
+	grpcPrometheus "github.com/grpc-ecosystem/go-grpc-prometheus"
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
 	"github.com/sirupsen/logrus"
-	"go.opentelemetry.io/contrib/instrumentation/google.golang.org/grpc/otelgrpc"
 	"go.opentelemetry.io/contrib/propagators/b3"
-	"go.opentelemetry.io/otel/attribute"
-	"google.golang.org/grpc/credentials/insecure"
+	"google.golang.org/grpc/reflection"
+	matchfunctiongrpc "plugin-arch-grpc-server-go/pkg/pb"
 
+	"go.opentelemetry.io/contrib/instrumentation/google.golang.org/grpc/otelgrpc"
 	"go.opentelemetry.io/otel"
+	"go.opentelemetry.io/otel/attribute"
 	"go.opentelemetry.io/otel/exporters/otlp/otlptrace/otlptracegrpc"
 	"go.opentelemetry.io/otel/propagation"
+	"go.opentelemetry.io/otel/sdk/metric"
 	"go.opentelemetry.io/otel/sdk/resource"
 	sdktrace "go.opentelemetry.io/otel/sdk/trace"
 	semconv "go.opentelemetry.io/otel/semconv/v1.12.0"
 
-	matchfunctiongrpc "plugin-arch-grpc-server-go/pkg/pb"
 	"plugin-arch-grpc-server-go/pkg/server"
 
 	"google.golang.org/grpc"
-	"google.golang.org/grpc/reflection"
+	"google.golang.org/grpc/credentials/insecure"
 )
 
 const (
-	service     = "custommatchmakingfunctiongoserver"
 	environment = "production"
 	id          = 1
 )
 
-var port = flag.Int("port", 6565, "The grpc server port")
+var (
+	service = os.Getenv("OTEL_SERVICE_NAME")
+	port    = flag.Int("port", 6565, "The grpc server port")
+	res     = resource.NewWithAttributes(
+		semconv.SchemaURL,
+		semconv.ServiceNameKey.String(service),
+		attribute.String("environment", environment),
+		attribute.Int64("ID", id),
+	)
+)
 
-func initProvider() (*sdktrace.TracerProvider, error) {
+func initProvider(ctx context.Context, grpcServer *grpc.Server) (*sdktrace.TracerProvider, error) {
 	// Create the Tempo exporter
-	ctx, cancel := context.WithCancel(context.Background())
-	defer cancel()
-
-	ctx, cancel = context.WithTimeout(ctx, time.Second)
-	defer cancel()
 	conn, err := grpc.DialContext(ctx, "tempo:4317", grpc.WithTransportCredentials(insecure.NewCredentials()), grpc.WithBlock())
 	if err != nil {
 		return nil, fmt.Errorf("failed to create gRPC connection to collector: %w", err)
@@ -63,12 +71,7 @@ func initProvider() (*sdktrace.TracerProvider, error) {
 	bsp := sdktrace.NewBatchSpanProcessor(traceExporter)
 	tracerProvider := sdktrace.NewTracerProvider(
 		sdktrace.WithSampler(sdktrace.AlwaysSample()),
-		sdktrace.WithResource(resource.NewWithAttributes(
-			semconv.SchemaURL,
-			semconv.ServiceNameKey.String(service),
-			attribute.String("environment", environment),
-			attribute.Int64("ID", id),
-		)),
+		sdktrace.WithResource(res),
 		sdktrace.WithSpanProcessor(bsp),
 	)
 	otel.SetTracerProvider(tracerProvider)
@@ -76,48 +79,63 @@ func initProvider() (*sdktrace.TracerProvider, error) {
 	// set global propagator to tracecontext (the default is no-op).
 	otel.SetTextMapPropagator(propagation.TraceContext{})
 
+	// metrics
+	enableMetrics(ctx, grpcServer)
+
 	// Shutdown will flush any remaining spans and shut down the exporter.
 	return tracerProvider, nil
 }
 
-func main() {
-	tp, err := initProvider()
+func enableMetrics(ctx context.Context, grpcServer *grpc.Server) {
+	// The exporter embeds a default OpenTelemetry Reader and
+	// implements prometheus.Collector, allowing it to be used as
+	// both a Reader and Collector.
+	rdr := metric.NewManualReader()
+	provider := metric.NewMeterProvider(metric.WithReader(rdr))
+	meter := provider.Meter("plugin-arch-grpc-server-instrumentation")
+
+	// Start the prometheus HTTP server and pass the exporter Collector to it
+	go serveMetrics(grpcServer)
+
+	// This is the equivalent of prometheus.NewHistogramVec
+	histogram, err := meter.SyncFloat64().Histogram("histogram")
 	if err != nil {
-		log.Fatal(err)
+		logrus.Fatal(err)
 	}
+	startTime := time.Now()
+	histogram.Record(ctx, float64(time.Since(startTime).Milliseconds()))
 
-	// Register our TracerProvider as the global so any imported
-	// instrumentation in the future will default to using it.
-	otel.SetTracerProvider(tp)
-	otel.SetTextMapPropagator(propagation.NewCompositeTextMapPropagator(
-		propagation.TraceContext{},
-		propagation.Baggage{},
-	))
+	ctx, _ = signal.NotifyContext(ctx, os.Interrupt)
+	<-ctx.Done()
+}
 
+func serveMetrics(grpcServer *grpc.Server) {
+	// Create a HTTP server for prometheus.
+	reg := prometheus.NewRegistry()
+	httpServer := &http.Server{Handler: promhttp.HandlerFor(reg, promhttp.HandlerOpts{}), Addr: "0.0.0.0:8080"}
+	logrus.Printf("serving metrics at localhost:8080/metrics")
+
+	// Create some standard server metrics.
+	grpcMetrics := grpcPrometheus.NewServerMetrics()
+
+	// Initialize all metrics.
+	grpcMetrics.InitializeMetrics(grpcServer)
+
+	// Start your http server for prometheus.
+	logrus.Printf("listen and serve for prometheus")
+	go func() {
+		if err := httpServer.ListenAndServe(); err != nil {
+			logrus.Fatalf("Unable to start a http server. %s", err.Error())
+		}
+	}()
+
+	logrus.Printf("listen and served")
+}
+
+func main() {
+	logrus.Infof("starting app server.")
 	ctx, cancel := context.WithCancel(context.Background())
 	defer cancel()
-
-	// Cleanly shutdown and flush telemetry when the application exits.
-	defer func(ctx context.Context) {
-		// Do not make the application hang when it is shutdown.
-		ctx, cancel = context.WithTimeout(ctx, time.Second*5)
-		defer cancel()
-		if err := tp.Shutdown(ctx); err != nil {
-			log.Fatal(err)
-		}
-	}(ctx)
-
-	tr := tp.Tracer("server-component-main")
-
-	_, span := tr.Start(ctx, "main")
-	span.End()
-
-	flag.Parse()
-
-	lis, err := net.Listen("tcp", fmt.Sprintf(":%d", *port))
-	if err != nil {
-		logrus.Fatalf("failed to listen: %v", err)
-	}
 
 	opts := []grpc.ServerOption{
 		grpc.ChainUnaryInterceptor(
@@ -131,6 +149,14 @@ func main() {
 
 	s := grpc.NewServer(opts...)
 
+	logrus.Infof("listening to grpc port.")
+	lis, err := net.Listen("tcp", fmt.Sprintf(":%d", *port))
+	if err != nil {
+		logrus.Fatalf("failed to listen: %v", err)
+
+		return
+	}
+
 	// propagator for envoy
 	header := http.Header{}
 	propagator := b3.New(b3.WithInjectEncoding(b3.B3MultipleHeader))
@@ -142,10 +168,49 @@ func main() {
 	matchfunctiongrpc.RegisterMatchFunctionServer(s, &server.MatchFunctionServer{
 		MatchMaker: matchMaker,
 	})
+	logrus.Infof("adding the grpc reflection.")
 	reflection.Register(s) // self documentation for the server
 	logrus.Printf("gRPC server listening at %v", lis.Addr())
 
-	if err = s.Serve(lis); err != nil {
-		logrus.Fatalf("failed to serve: %v", err)
+	logrus.Infof("listening...")
+	go func() {
+		if err = s.Serve(lis); err != nil {
+			logrus.Fatalf("failed to serve: %v", err)
+
+			return
+		}
+	}()
+
+	logrus.Infof("starting init provider.")
+	tp, err := initProvider(ctx, s)
+	if err != nil {
+		logrus.Fatalf("failed to initializing the provider. %s", err.Error())
+
+		return
 	}
+
+	// Register our TracerProvider as the global so any imported
+	// instrumentation in the future will default to using it.
+	otel.SetTracerProvider(tp)
+	otel.SetTextMapPropagator(propagation.NewCompositeTextMapPropagator(
+		propagation.TraceContext{},
+		propagation.Baggage{},
+	))
+
+	// Cleanly shutdown and flush telemetry when the application exits.
+	defer func(ctx context.Context) {
+		// Do not make the application hang when it is shutdown.
+		ctx, cancel = context.WithTimeout(ctx, time.Second*5)
+		defer cancel()
+		if err := tp.Shutdown(ctx); err != nil {
+			logrus.Fatal(err)
+		}
+	}(ctx)
+
+	tr := tp.Tracer("server-component-main")
+
+	_, span := tr.Start(ctx, "main")
+	span.End()
+
+	flag.Parse()
 }
