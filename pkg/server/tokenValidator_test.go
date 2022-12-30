f222201d14890f2a59cbcd95524a70cf786bbcc0
ruli <ruli@accelbyte.net>
2022-12-30 08:21:31 +0700

    fix: envar test

--- pkg/server/tokenValidator_test.go
+++ pkg/server/tokenValidator_test.go
@@ -11,14 +11,30 @@ import (
 
 	"github.com/AccelByte/accelbyte-go-sdk/services-api/pkg/factory"
 	"github.com/AccelByte/accelbyte-go-sdk/services-api/pkg/service/iam"
-	"github.com/stretchr/testify/assert"
-
 	sdkAuth "github.com/AccelByte/accelbyte-go-sdk/services-api/pkg/utils/auth"
+	"github.com/stretchr/testify/assert"
 )
+
+type MyConfigRepo struct {
+	baseUrl      string
+	clientId     string
+	clientSecret string
+}
+
+func (c *MyConfigRepo) GetClientId() string       { return c.clientId }
+func (c *MyConfigRepo) GetClientSecret() string   { return c.clientSecret }
+func (c *MyConfigRepo) GetJusticeBaseUrl() string { return c.baseUrl }
 
 func TestTokenValidator_ValidateToken(t *testing.T) {
 	// Arrange
-	configRepo := sdkAuth.DefaultConfigRepositoryImpl()
+	baseUrl := "https://development.accelbyte.io"
+	clientId := "4eb9c4f862fe452e8d675c3030f25912" // TODO mock and remove hardcode
+	clientSecret := ""
+	configRepo := &MyConfigRepo{
+		baseUrl:      baseUrl,
+		clientId:     clientId,
+		clientSecret: clientSecret,
+	}
 	tokenRepo := sdkAuth.DefaultTokenRepositoryImpl()
 	authService := iam.OAuth20Service{
 		Client:           factory.NewIamClient(configRepo),
@@ -26,7 +42,7 @@ func TestTokenValidator_ValidateToken(t *testing.T) {
 		TokenRepository:  tokenRepo,
 	}
 
-	err := authService.LoginClient(&configRepo.ClientId, &configRepo.ClientSecret)
+	err := authService.LoginClient(&clientId, &clientSecret)
 	if err != nil {
 		assert.Fail(t, err.Error())
 		return
