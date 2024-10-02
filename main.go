35dac2f2df9c98b7eb53d021d2b60a1c42de1748
ruli sastra putri <ruli@accelbyte.net>
2024-10-02 16:17:27 +0700

    chore: use the same format as other

--- main.go
+++ main.go
@@ -13,17 +13,15 @@ import (
 	_ "net/http/pprof"
 	"os/signal"
 	"runtime"
-	"strings"
 	"syscall"
 
-	"github.com/AccelByte/accelbyte-go-sdk/services-api/pkg/factory"
-	"github.com/AccelByte/accelbyte-go-sdk/services-api/pkg/service/iam"
-	sdkAuth "github.com/AccelByte/accelbyte-go-sdk/services-api/pkg/utils/auth"
 	promgrpc "github.com/grpc-ecosystem/go-grpc-middleware/providers/prometheus"
+	"github.com/grpc-ecosystem/go-grpc-middleware/v2/interceptors/logging"
 	"github.com/prometheus/client_golang/prometheus"
 	"github.com/prometheus/client_golang/prometheus/collectors"
 	"github.com/prometheus/client_golang/prometheus/promhttp"
 	"github.com/sirupsen/logrus"
+	"go.opentelemetry.io/otel/trace"
 
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/health"
@@ -41,18 +39,19 @@ import (
 )
 
 const (
-	environment = "production"
-	id          = 1
+	environment     = "production"
+	id              = int64(1)
+	metricsEndpoint = "/metrics"
+	metricsPort     = 8080
+	grpcPort        = 6565
 )
 
 var (
-	port        = flag.Int("port", 6565, "The grpc server port")
 	serviceName = common.GetEnv("OTEL_SERVICE_NAME", "MatchmakingFunctionGrpcPluginServerGoDocker")
+	logLevelStr = common.GetEnv("LOG_LEVEL", logrus.InfoLevel.String())
 )
 
 func main() {
-	configureLogging()
-
 	go func() {
 		runtime.SetBlockProfileRate(1)
 		runtime.SetMutexProfileFraction(10)
@@ -61,33 +60,36 @@ func main() {
 	ctx, cancel := context.WithCancel(context.Background())
 	defer cancel()
 
-	scope := common.NewRootScope(ctx, serviceName, common.GenerateUUID())
-	defer scope.Finish()
+	logrus.Infof("starting app server.")
 
-	scope.Log.Infof("starting app server.")
+	logrusLevel, err := logrus.ParseLevel(logLevelStr)
+	if err != nil {
+		logrusLevel = logrus.InfoLevel
+	}
+	logrusLogger := logrus.New()
+	logrusLogger.SetLevel(logrusLevel)
+
+	loggingOptions := []logging.Option{
+		logging.WithLogOnEvents(logging.StartCall, logging.FinishCall, logging.PayloadReceived, logging.PayloadSent),
+		logging.WithFieldsFromContext(func(ctx context.Context) logging.Fields {
+			if span := trace.SpanContextFromContext(ctx); span.IsSampled() {
+				return logging.Fields{"traceID", span.TraceID().String()}
+			}
+
+			return nil
+		}),
+		logging.WithLevels(logging.DefaultClientCodeToLevel),
+		logging.WithDurationField(logging.DurationToDurationField),
+	}
 
 	srvMetrics := promgrpc.NewServerMetrics()
 	unaryServerInterceptors := []grpc.UnaryServerInterceptor{
 		srvMetrics.UnaryServerInterceptor(),
+		logging.UnaryServerInterceptor(common.InterceptorLogger(logrusLogger), loggingOptions...),
 	}
 	streamServerInterceptors := []grpc.StreamServerInterceptor{
 		srvMetrics.StreamServerInterceptor(),
-	}
-
-	if strings.ToLower(common.GetEnv("PLUGIN_GRPC_SERVER_AUTH_ENABLED", "false")) == "true" {
-		configRepo := sdkAuth.DefaultConfigRepositoryImpl()
-		tokenRepo := sdkAuth.DefaultTokenRepositoryImpl()
-		server.OAuth = &iam.OAuth20Service{
-			Client:           factory.NewIamClient(configRepo),
-			ConfigRepository: configRepo,
-			TokenRepository:  tokenRepo,
-		}
-
-		server.OAuth.SetLocalValidation(true)
-
-		unaryServerInterceptors = append(unaryServerInterceptors, server.UnaryAuthServerIntercept)
-		streamServerInterceptors = append(streamServerInterceptors, server.StreamAuthServerIntercept)
-		scope.Log.Infof("added auth interceptors")
+		logging.StreamServerInterceptor(common.InterceptorLogger(logrusLogger), loggingOptions...),
 	}
 
 	// Create gRPC Server
@@ -105,7 +107,7 @@ func main() {
 
 	// Enable gRPC Reflection
 	reflection.Register(grpcServer)
-	scope.Log.Infof("gRPC reflection enabled")
+	logrus.Infof("gRPC reflection enabled")
 
 	// Enable gRPC Health Check
 	grpc_health_v1.RegisterHealthServer(grpcServer, health.NewServer())
@@ -120,36 +122,36 @@ func main() {
 	)
 
 	go func() {
-		http.Handle("/metrics", promhttp.HandlerFor(promRegistry, promhttp.HandlerOpts{}))
-		scope.Log.Fatal(http.ListenAndServe(":8080", nil))
+		http.Handle(metricsEndpoint, promhttp.HandlerFor(promRegistry, promhttp.HandlerOpts{}))
+		logrus.Fatal(http.ListenAndServe(fmt.Sprintf(":%d", metricsPort), nil))
 	}()
-	scope.Log.Printf("prometheus metrics served at :8080/metrics")
+	logrus.Printf("prometheus metrics served at :8080/metrics")
 
-	scope.Log.Infof("listening to grpc port.")
-	lis, err := net.Listen("tcp", fmt.Sprintf(":%d", *port))
+	logrus.Infof("listening to grpc port.")
+	lis, err := net.Listen("tcp", fmt.Sprintf(":%d", grpcPort))
 	if err != nil {
-		scope.Log.Fatalf("failed to listen: %v", err)
+		logrus.Fatalf("failed to listen: %v", err)
 
 		return
 	}
 
-	scope.Log.Printf("gRPC server listening at %v", lis.Addr())
+	logrus.Printf("gRPC server listening at %v", lis.Addr())
 
-	scope.Log.Infof("listening...")
+	logrus.Infof("listening...")
 	go func() {
 		if err = grpcServer.Serve(lis); err != nil {
-			scope.Log.Fatalf("failed to serve: %v", err)
+			logrus.Fatalf("failed to serve: %v", err)
 
 			return
 		}
 	}()
 
-	scope.Log.Infof("starting init provider.")
+	logrus.Infof("starting init provider.")
 
 	// Save Tracer Provider
 	tracerProvider, err := common.NewTracerProvider(serviceName, environment, id)
 	if err != nil {
-		scope.Log.Fatalf("failed to create tracer provider: %v", err)
+		logrus.Fatalf("failed to create tracer provider: %v", err)
 
 		return
 	}
@@ -168,7 +170,7 @@ func main() {
 	// Cleanly shutdown and flush telemetry when the application exits.
 	defer func(ctx context.Context) {
 		if err := tracerProvider.Shutdown(ctx); err != nil {
-			scope.Log.Fatal(err)
+			logrus.Fatal(err)
 		}
 	}(ctx)
 
@@ -176,16 +178,4 @@ func main() {
 
 	ctx, _ = signal.NotifyContext(ctx, syscall.SIGINT, syscall.SIGTERM)
 	<-ctx.Done()
-	scope.Log.Println("Goodbye...")
-}
-
-func configureLogging() logrus.Level {
-	logLevel, err := logrus.ParseLevel(common.GetEnv("LOG_LEVEL", logrus.InfoLevel.String()))
-	if err != nil {
-		logrus.Error("unable to parse log level: ", err)
-	}
-	logrus.SetLevel(logLevel)
-	logrus.SetReportCaller(true)
-
-	return logLevel
 }
