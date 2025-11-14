b924447bf861739ee1388c944588557621abfa07
Elmer Nocon <elmernocon@gmail.com>
2025-11-14 14:27:12 +0800

    chore: add dev container support

--- proto.sh
+++ proto.sh
@@ -4,20 +4,26 @@ set -eou pipefail
 
 shopt -s globstar
 
+find_all_proto_files() {
+  find "${PROTO_DIR}" -name "*.proto" -type f
+}
+
+clean_generated_files() {
+  find "$1" -type f -name '*pb.go' -delete
+}
+
 PROTO_DIR="${1:-pkg/proto}"
 OUT_DIR="${2:-pkg/pb}"
 
-# Ensure output directory exists
-mkdir -p "${OUT_DIR}"
+# Clean previously generated files.
+mkdir -p "${OUT_DIR:?}" && \
+  clean_generated_files "${OUT_DIR}"
 
-# Clean previously generated files
-find "${OUT_DIR}" -type f \( -name '*.go' \) -delete
-
-# Generate protobuf files.
-protoc-wrapper \
-  --proto_path="${PROTO_DIR}" \
+# Step 1: Generate Go code for ALL proto files
+protoc \
+  -I "${PROTO_DIR}" \
   --go_out="${OUT_DIR}" \
   --go_opt=paths=source_relative \
   --go-grpc_out="${OUT_DIR}" \
-  --go-grpc_opt=paths=source_relative \
-  "${PROTO_DIR}"/**/*.proto
+  --go-grpc_opt=paths=source_relative,require_unimplemented_servers=false \
+  $(find_all_proto_files)
